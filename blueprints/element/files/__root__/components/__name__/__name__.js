41b0d6a1d56eb8383c6971a3c3b697321de64211
Onasenko Sergey <onasenko.sergey@gmail.com>
2016-10-23 03:42:09 +0300

    chore(blueprints): add element component blueprint
    element represents function component with custom styles

---
+++ blueprints/element/files/__root__/components/__name__/__name__.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './<%= pascalEntityName %>.scss'
+
+export const <%= pascalEntityName %> = () => (
+  <div className="<%= dashesEntityName %>">
+    <h3><%= pascalEntityName %></h3>
+  </div>
+)
+
+export default <%= pascalEntityName %>
