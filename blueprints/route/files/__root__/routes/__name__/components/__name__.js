83254677be3cde968591fecfc0c630d33d1f0216
Onasenko Sergey <onasenko.sergey@gmail.com>
2016-10-23 03:45:36 +0300

    chore(blueprints): add route component blueprint
    route represents asynchronously loaded page, including own dumb component, styles, smart component and module

---
+++ blueprints/route/files/__root__/routes/__name__/components/__name__.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './<%= pascalEntityName %>.scss'
+
+export const <%= pascalEntityName %> = () => (
+  <div className="<%= dashesEntityName %>">
+    <h4><%= pascalEntityName %></h4>
+  </div>
+)
+
+export default <%= pascalEntityName %>
---
+++ blueprints/route/files/__root__/routes/__name__/containers/__name__.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux'
+// import { bindActionCreators } from 'redux'
+// import { actions } from '../modules/<%= pascalEntityName %>'
+
+import <%= pascalEntityName %> from '../components/<%= pascalEntityName %>'
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = (dispatch) => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(<%= pascalEntityName %>)
