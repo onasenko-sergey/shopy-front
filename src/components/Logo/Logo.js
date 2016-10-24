f7a8a3e49c6d511e8cc092d98c7c174a3e596486
Onasenko Sergey <onasenko.sergey@gmail.com>
2016-10-25 00:07:25 +0300

    feature(logo): shopy brand image

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Logo.scss'
+import LogoImg from './assets/logo.png'
+
+export const Logo = () => (
+  <img src={LogoImg} alt='Shopy brand image' className='logo img-responsive' />
+)
+
+export default Logo
