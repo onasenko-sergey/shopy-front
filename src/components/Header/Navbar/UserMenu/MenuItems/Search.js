3f28254ebfea79982c3d8496b4a57479c4276e70
Onasenko Sergey <onasenko.sergey@gmail.com>
2016-10-27 23:06:12 +0300

    feature(user menu): create search menu item

---
+++ src/components/Header/Navbar/UserMenu/MenuItems/Search.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react'
+import Icons from 'components/Icons'
+
+const propTypes = {
+  searchBar: PropTypes.object.isRequired,
+  searchBarActions: PropTypes.objectOf(PropTypes.func).isRequired
+}
+
+class SearchItem extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (event) {
+    event.preventDefault()
+    const { searchBar, searchBarActions } = this.props
+    // when searchbar is opened, click on search menu icon leads to search form submit
+    if (searchBar.isOpened) {
+      searchBar.formRef.submit()
+    } else {
+      searchBarActions.openSearch()
+    }
+  }
+
+  render () {
+    const { searchBar } = this.props
+    return (
+      <li className='user-menu__item'>
+        <a href='/search' onClick={this.handleClick}>
+          <Icons.Search highlighted={searchBar.isOpened} />
+        </a>
+      </li>
+    )
+  }
+}
+
+SearchItem.propTypes = propTypes
+
+export default SearchItem
+
