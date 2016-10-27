36687c02834fc967a785cf13109023e5ee0b865b
Onasenko Sergey <onasenko.sergey@gmail.com>
2016-10-27 23:27:01 +0300

    feature(state): connect user, search, form reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,12 +2,22 @@ import { combineReducers } from 'redux'
 import locationReducer from './location'
 import modalsReducer from 'redux/modules/Modals'
 import errorsReducer from 'redux/modules/Errors'
+import userReducer from 'redux/modules/User'
+import { reducer as formReducer } from 'redux-form'
+import searchBar from 'redux/modules/SearchBar'
+import searchResults from 'redux/modules/SearchResults'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
     location: locationReducer,
     modals: modalsReducer,
     errors: errorsReducer,
+    user: userReducer,
+    form: formReducer,
+    search: combineReducers({
+      bar: searchBar,
+      results: searchResults
+    }),
     ...asyncReducers
   })
 }
