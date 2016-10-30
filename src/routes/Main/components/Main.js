f13a6b17b5ae8b36c8b3fbd55281c0c92132a0f0
Onasenko Sergey <onasenko.sergey@gmail.com>
2016-10-30 15:48:59 +0300

    feature(main_page): fetch main page data

--- src/routes/Main/components/Main.js
+++ src/routes/Main/components/Main.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import './Main.scss'
 
-export const Main = () => (
-  <div className='main'>
-    <h4>Main</h4>
-  </div>
-)
+const propTypes = {
+  init: PropTypes.func.isRequired
+}
 
+class Main extends Component {
+  componentWillMount () {
+    const { init } = this.props
+    init(true)
+  }
+
+  render () {
+    return (
+      <div className='main'>
+        <h4>Main</h4>
+      </div>
+    )
+  }
+}
+
+Main.propTypes = propTypes
 export default Main
--- src/routes/Main/containers/Main.js
+++ src/routes/Main/containers/Main.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux'
 // import { bindActionCreators } from 'redux'
-// import { actions } from '../modules/Main'
+import { actions } from '../modules/Main'
 
 import Main from '../components/Main'
 
-const mapStateToProps = (state) => ({})
+/* eslint-disable camelcase */
+const mapStateToProps = ({ main_page }) => ({ ...main_page })
+/* eslint-enable camelcase */
 
-const mapDispatchToProps = (dispatch) => ({})
+// const mapDispatchToProps = (dispatch) => ({})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main)
+export default connect(mapStateToProps, actions)(Main)
